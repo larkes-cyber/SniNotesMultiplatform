dae11d500c4f684d2632395036977afe9a969ae4
macbook <playegrovoy@gmail.com>
2023-09-05 11:42:12 +0300

    login screen ios

--- iosApp/iosApp/login_screen/LoginScreen.swift
+++ iosApp/iosApp/login_screen/LoginScreen.swift
@@ -12,21 +12,25 @@ import SwiftUI
 struct LoginScreen:View{
     
     @StateObject var viewModel = LoginScreenViewModel()
+    @State private(set) var isSignUp = false
     
     var body: some View {
         VStack{
-            Text("Sign up now")
+            Text(isSignUp ? "Sign up now" : "Sign in now" )
                 .font(.system(size: 32, weight: .medium))
                 .padding(.bottom, 14)
-            Text("Please fill the details and create account")
-                .font(.system(size: 18, weight: .medium))
-                .frame(width: 330)
-                .multilineTextAlignment(.center)
-                .padding(.bottom, 40)
-            TextField("Enter your name", text: $viewModel.name)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-                .padding(.horizontal, 30)
-                .padding(.bottom, 10)
+            if isSignUp{
+                Text("Please fill the details and create account")
+                    .font(.system(size: 18, weight: .medium))
+                    .frame(width: 330)
+                    .multilineTextAlignment(.center)
+                    .padding(.bottom, 40)
+                TextField("Enter your name", text: $viewModel.name)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding(.horizontal, 30)
+                    .padding(.bottom, 10)
+            }
+            
             TextField("Enter your email", text: $viewModel.login)
                 .textFieldStyle(RoundedBorderTextFieldStyle())
                 .padding(.horizontal, 30)
@@ -36,6 +40,23 @@ struct LoginScreen:View{
                 .padding(.horizontal, 30)
                 .padding(.bottom, 10)
 
+            LargeButton(
+                title:isSignUp ? "Sign up" : "Sign in",
+                backgroundColor: Color.blue) {
+                }
+                .padding(.bottom, 10)
+                .padding(.horizontal, 10)
+            
+            HStack{
+                Text(isSignUp ? "Already have an account" : "Don't have an account")
+                    .font(.system(size: 12, weight: .medium))
+                Button(isSignUp ? "Sign in" : "Sign up"){
+                    isSignUp = !isSignUp
+                }
+                .font(.system(size: 12, weight: .medium))
+                
+            }
+            
             
             
         }
@@ -47,3 +68,6 @@ struct LoginScreen_Previews: PreviewProvider {
         LoginScreen()
     }
 }
+
+
+
